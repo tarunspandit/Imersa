a0444a9e4834d19332e46df8bc09dad4f851831e
Tarun Pandit <tpandit@gmu.edu>
2025-08-26 17:38:48 -0400

    l

--- BridgeEmulator/githubInstall.sh
+++ BridgeEmulator/githubInstall.sh
@@ -47,16 +47,10 @@ if [ $2 = allreadytoinstall ]; then
     chmod +x /opt/hue-emulator/genCert.sh
     rm -r diyHue-master
 else
-    echo "ui update"
+    echo "ui update - skipped (using React UI)"
 fi
 
-mkdir diyhueUI
-curl -sL https://github.com/diyhue/diyHueUI/releases/latest/download/DiyHueUI-release.zip -o diyHueUI.zip
-#curl -sL https://github.com/hendriksen-mark/diyHueUI/releases/latest/download/DiyHueUI-release.zip -o diyHueUI.zip
-unzip -qo diyHueUI.zip -d diyhueUI
-rm diyHueUI.zip
-cp -r diyhueUI/dist/index.html /opt/hue-emulator/flaskUI/templates/
-cp -r diyhueUI/dist/assets /opt/hue-emulator/flaskUI/
-rm -r diyhueUI
+# Skip downloading old UI - we're using React UI now
+echo "React UI is already installed at /opt/hue-emulator/react-ui"
 
 curl -s $1/restart
